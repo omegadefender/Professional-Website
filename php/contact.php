f5dc87d85435444928ceb1f58eb9d26ade1f3426
James Hastings <hastings.james.m@gmail.com>
2021-01-22 20:18:26 +1300

    heroku setup

--- php/contact.php
+++ php/contact.php
@@ -3,7 +3,7 @@ $first_name = '';
 $last_name = '';
 $emailaddress = '';
 $to = 'hastings.james.m@gmail.com';
-$subject = '';
+$subject = getenv('SUBJECT');
 $message  = '';
 $headers = '';
 
@@ -11,7 +11,6 @@ if(isset($_POST['submit'])) {
     $first_name = htmlspecialchars($_POST["firstname"]);
     $last_name = htmlspecialchars($_POST["lastname"]);
     $emailaddress = htmlspecialchars($_POST["emailaddress"]);
-    $subject = htmlspecialchars($_POST["subject"]);
     $message = htmlspecialchars($_POST["body"]);
     $headers = "Message sent from $emailaddress";
     mail($to, $subject, $message, $headers);
