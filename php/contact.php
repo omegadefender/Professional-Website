e4fd5694e27e3cd59502f59ccc3cebed5e484011
James Hastings <hastings.james.m@gmail.com>
2021-01-25 20:12:00 +1300

    mail being sent via mailgun

--- php/contact.php
+++ php/contact.php
@@ -1,47 +1,46 @@
 <?php
 
-// Load Composer's autoloader
 require '..\vendor\autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+$first_name = '';
+$last_name = '';
+$emailaddress = '';
+$message = '';
+
+if(isset($_POST['submit'])) {
+    $first_name = htmlspecialchars($_POST["firstname"]);
+    $last_name = htmlspecialchars($_POST["lastname"]);
+    $emailaddress = htmlspecialchars($_POST["emailaddress"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["body"]);
+}
+
 $mail = new PHPMailer(true);
+$address = getenv('GMAIL_USERNAME');
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output
+    $mail->isSMTP(); // Send using SMTP
+    $mail->Host       = 'smtp.mailgun.org'; // Set the SMTP server to send through
+    $mail->SMTPAuth   = true; // Enable SMTP authentication
+    $mail->Username   = getenv("MAILGUN_SMTP_LOGIN"); // SMTP username
+    $mail->Password   = getenv('MAILGUN_SMTP_PASSWORD'); // SMTP password
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = getenv("GMAIL_USERNAME");                     // SMTP username
-    $mail->Password   = getenv('GMAIL_PASSWORD');                     // SMTP password
-    $mail->SMTPSecure = 'PHPMailer::ENCRYPTION_SMTPS'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged. Default - 'PHPMailer::ENCRYPTION_STARTTLS'
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->From = getenv('GMAIL_USERNAME');
+    $mail->FromName = 'Mailgun';
+    $mail->addAddress(getenv('GMAIL_USERNAME')); // Add a recipient
 
-    //Recipients
-    $mail->setFrom('hastings.james.m@gmail.com', 'Mailer');
-    $mail->addAddress('psalm69er@gmail.com', 'Joe User');     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
-
-    // Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-    // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = getenv("SUBJECT");
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = $subject;
+    $mail->Body    = "Hi James <br> You got a new message from $first_name $last_name<br><br>Who wrote...<br> $message<br>Their email
+     address: $emailaddress";
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    echo $mail->Password;
-}
+
 ?>
