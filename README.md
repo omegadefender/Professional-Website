905ae308fb18bc50d155ac8c0adff548a375d9d2
James Hastings <hastings.james.m@gmail.com>
2021-01-27 01:07:09 +1300

    refactored php and improved html

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ https://floating-ocean-41543.herokuapp.com/
 ### **Description**
 
 This is my personal website that I use for practising my web development skills.
-Built using HTML, CSS, and PHP.
+
+Built using HTML, CSS, and PHP. Deployed on Heroku
--- php/contact.php
+++ php/contact.php
@@ -1,19 +1,12 @@
 <?php
-
 require '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-$first_name = '';
-$last_name = '';
-$emailaddress = '';
-$message = '';
-
 if(isset($_POST['submit'])) {
-    $first_name = htmlspecialchars($_POST["firstname"]);
-    $last_name = htmlspecialchars($_POST["lastname"]);
+    $name = htmlspecialchars($_POST["name"]);
     $emailaddress = htmlspecialchars($_POST["emailaddress"]);
     $subject = htmlspecialchars($_POST["subject"]);
     $message = htmlspecialchars($_POST["body"]);
@@ -22,25 +15,21 @@ if(isset($_POST['submit'])) {
 $mail = new PHPMailer(true);
 $address = getenv('GMAIL_USERNAME');
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output
-    $mail->isSMTP(); // Send using SMTP
-    $mail->Host       = 'smtp.mailgun.org'; // Set the SMTP server to send through
-    $mail->SMTPAuth   = true; // Enable SMTP authentication
-    $mail->Username   = getenv("MAILGUN_SMTP_LOGIN"); // SMTP username
-    $mail->Password   = getenv('MAILGUN_SMTP_PASSWORD'); // SMTP password
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.mailgun.org';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = getenv("MAILGUN_SMTP_LOGIN");
+    $mail->Password   = getenv('MAILGUN_SMTP_PASSWORD');
     $mail->SMTPSecure = 'tls';
-    $mail->Port       = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    $mail->From = getenv('GMAIL_USERNAME');
-    $mail->FromName = 'Mailgun';
+    $mail->Port       = getenv('MAILGUN_SMTP_PORT');
+    $mail->From       = getenv('GMAIL_USERNAME');
+    $mail->FromName   = 'Mailgun';
     $mail->addAddress(getenv('GMAIL_USERNAME')); // Add a recipient
-
-    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->isHTML(true);
     $mail->Subject = $subject;
-    $mail->Body    = "Hi James <br> You got a new message from $first_name $last_name<br><br>Who wrote...<br> $message<br>Their email
+    $mail->Body    = "Hi James <br> You got a new message from $name<br><br>Who wrote...<br> $message<br>Their email
      address: $emailaddress";
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
     $mail->send();
-    echo 'Message has been sent';
-
+    echo 'Message has been sent<br><br>Click <a href="../index.html">here</a> to go back to the homepage';
 ?>
