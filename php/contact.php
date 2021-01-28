d616da335491a2d988dd5a3fcdad5cf1529f20ea
James Hastings <hastings.james.m@gmail.com>
2021-01-28 16:25:52 +1300

    php validation on name field, a little refactoring

--- php/contact.php
+++ php/contact.php
@@ -1,4 +1,5 @@
 <?php
+
 require '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -7,7 +8,7 @@ use PHPMailer\PHPMailer\Exception;
 
 if(isset($_POST['submit'])) {
     $name = htmlspecialchars($_POST["name"]);
-    $emailaddress = htmlspecialchars($_POST["emailaddress"]);
+    $emailaddress = htmlspecialchars($_POST["email"]);
     $subject = htmlspecialchars($_POST["subject"]);
     $message = htmlspecialchars($_POST["body"]);
 }
@@ -23,13 +24,20 @@ $address = getenv('GMAIL_USERNAME');
     $mail->SMTPSecure = 'tls';
     $mail->Port       = getenv('MAILGUN_SMTP_PORT');
     $mail->From       = getenv('GMAIL_USERNAME');
-    $mail->FromName   = 'Mailgun';
+    $mail->FromName   = 'number8websites.com';
     $mail->addAddress(getenv('GMAIL_USERNAME')); // Add a recipient
     $mail->isHTML(true);
     $mail->Subject = $subject;
-    $mail->Body    = "Hi James <br> You got a new message from $name<br><br>Who wrote...<br> $message<br>Their email
-     address: $emailaddress";
+    $mail->Body    = "Hi James <br><br> You got a new message from: $name<br>Their email
+    address: $emailaddress<br>Who wrote...<br><br> $message";
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    $mail->send();
-    echo 'Message has been sent<br><br>Click <a href="../index.html">here</a> to go back to the homepage';
+    if(isset($_POST["name"]) && $name !== '') {
+        $name = htmlspecialchars($_POST["name"]);
+        $mail->send();
+        echo 'Message has been sent<br><br><a href="../index.html">Click here</a> to go back to the homepage';
+     } else {
+         echo 'Your message has NOT been sent!<br>
+         You forgot to fill in your name<br><br><a href="../html/contact.html">Click here</a> to go back to the form';
+     }
+
 ?>
