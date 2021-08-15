c937b7b4ea96253a78995f356500a346f1cbcce0
James Hastings <hastings.james.m@gmail.com>
2021-08-16 11:46:40 +1200

    refactored about page

--- javascript/generic.js
+++ javascript/generic.js
@@ -20,3 +20,18 @@ currentLink = () => {
         document.getElementById("contactButton").style.color = '#0088a9'
     }
 }
+
+hider = () => {
+    var me = document.getElementById("aboutme");
+    var img = document.getElementById("mysteryme");
+    var caption = document.getElementById("cap")
+    if (me.style.display === "none") {
+        me.style.display = "block";
+        img.src = "../images/Profile.png";
+        caption.innerHTML = "James Hastings";
+    } else {
+        me.style.display = "none";
+        img.src = "../images/mysteryman-min.jpg";
+        caption.innerHTML = "Click Image"
+    }
+}
